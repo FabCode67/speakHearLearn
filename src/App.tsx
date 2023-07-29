f2f75d6e2e78faac74ed5695d68945464ca29373
Fabrice <mwanafunzifabrice@gmail.com>
2023-07-30 00:50:10 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex bg-blue-900 justify-center items-center al m-auto text-center h-screen w-full">
+      <p className="font font-serif text-5xl text-white">Hello welcome to speak hear learn project</p>
+      </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: "#000",
+        white: "#fff",
+        gray: {
+            50: "#f9fafb",
+            100: "#f4f5f7",
+            200: "#e5e7eb",
+            300: "#d2d6dc",
+            400: "#9fa6b2",
+        },
+        blue:{
+            50: "#f0f9ff",
+            100: "#e0f2fe",
+            200: "#bae6fd",
+            300: "#7dd3fc",
+            400: "#38bdf8",
+            500: "#0ea5e9",
+            600: "#0284c7",
+            700: "#0369a1",
+            800: "#075985",
+            900: "#1581E5",
+        },
+        },
+    fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+    },
+
+    screens: {
+      xs: "320px",
+      md: "768px",
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+      "2xl": "1536px",
+      "3xl": "1920px",
+      "4xl": "2560px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
